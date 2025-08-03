2e49c314f0b489784ade7fc3b2e5316ee3359fac
Mac80Mo <marcus.moser@mail.de>
2025-08-03 12:57:35 +0200

    Erstelle die Grundstruktur für die Visitenkarte mit HTML, CSS und JavaScript

---
+++ 01 - Visitenkarte/card-ui.js
@@ -0,0 +1,11 @@
+const fullName =document.getElementById("fullName");
+fullName.innerText = "Marcus Moser";
+
+const job = document.getElementById("job");
+job.innerText = "Anwendungsentwickler";
+
+const tele = document.getElementById("tele");
+tele.innerText = "+49 123 456789";
+
+const email = document.getElementById("email");
+email.innerText = "marcus.moser@example.de";
---
+++ 01 - Visitenkarte/card-vanilla.js
@@ -0,0 +1,13 @@
+const firstName = "Marcus";
+const lastName = "Moser";
+const job = "Anwendungsentwickler";
+const tele = "+49 123 456789";
+const email = "marcus.moser@example.com";
+
+const person = `
+${firstName} ${lastName}
+${job}
+Tel.: ${tele}
+Email: ${email}`;
+
+console.log(person);
