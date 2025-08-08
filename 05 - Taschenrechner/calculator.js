e7cdd33662c2674e0f5d5b9dc966c0ca0ebd19c9
Mac80Mo <marcus.moser@mail.de>
2025-08-08 15:41:26 +0200

    Korrigiere die Update-Display-Funktion, um Eingaben korrekt anzuzeigen und füge Event-Listener für Tastatureingaben hinzu

--- 05 - Taschenrechner/calculator.js
+++ 05 - Taschenrechner/calculator.js
@@ -6,7 +6,7 @@ const clear = document.querySelector(".clear");
 
 // Funktion zum Anzeigen der Eingaben auf dem Display
 const updateDisplay = (buttonValue) => {
-    display.value = buttonValue;
+    display.value += buttonValue;
 };
 
 // Event-Listener für Klick auf Zahlen, Kommazeichen und Operatoren
@@ -15,3 +15,34 @@ number.forEach((numberElement) => {
         updateDisplay(numberElement.innerText);
     });
 })
+
+// Funktion zum Berechnen des Ausdrucks im Display
+const calculateExpression = () => {
+    // Diese Variante oist nicht besonders sicher || suche Parse )
+    display.value = eval(display.value);
+};
+
+// Event-LIstener für klicks auf Gleichheitszeichen (=)
+equals.addEventListener("click", calculateExpression);
+
+// Funktion zum Löschen des Display-Inhalts
+const clearDisplay = () => {
+    display.value = "";
+};
+
+// Event-Listener für Klick auf Clear-Button (C)
+clear.addEventListener("click", clearDisplay);
+
+
+// BONUS - Event-Listener für Tastatureingaben
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+
+    if (key === "Enter") {
+        calculateExpression();
+    } else if (key === "Escape") {
+        clearDisplay(); // RegEx
+    } else if (/^[0-9+\-*/.]$/.test(key)) {
+        updateDisplay(key);
+    }
+});
