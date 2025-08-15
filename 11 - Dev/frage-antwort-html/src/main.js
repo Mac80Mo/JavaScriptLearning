41073a46fb22b073448d2174a7a9791bf5cf45e0
Mac80Mo <marcus.moser@mail.de>
2025-08-15 20:35:09 +0200

    Implementiere Quizfragen und Punktevergabe in der Hauptlogik

--- 11 - Dev/frage-antwort-html/src/main.js
+++ 11 - Dev/frage-antwort-html/src/main.js
@@ -1,24 +1,78 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let points = 0;
+const questions = [
+  `Wie viel ist 2 + 2 ?
+a 4
+b 6
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  `Wie viel ist 2 + 4 ?
+a 4
+b 6
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  `Wie viel ist 2 * 4 ?
+a 6
+b 8
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  `Wie viel ist 8 * 3 ?
+a 16
+b 24
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  `Wie viel ist 24 / 2 ?
+a 12
+b 8
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const correctAnswers = ["a", "b", "b", "b", "a"];
+const pointsForCorrectAnswers = [5, 5, 10, 10, 10];
+let maxPoints = 0;
+let i = 0;
 
-setupCounter(document.querySelector('#counter'))
+// Frage
+const questionDiv = document.getElementById("question");
+questionDiv.innerText = questions[i];
+
+// Button
+const commitButton = document.getElementById("commit");
+commitButton.addEventListener("click", () => {
+  const answerInput = document.getElementById("answer");
+  const answer = answerInput.value;
+  const outputDiv = document.getElementById("output");
+
+  if (answer === correctAnswers[i]) {
+    outputDiv.innerText = "Richtig!";
+    points = points + pointsForCorrectAnswers[i];
+  } else {
+    outputDiv.innerText = "Falsch!";
+  }
+
+  // Wenn der User die Antwort gegeben hat, erhöhen wir...
+  maxPoints += pointsForCorrectAnswers[i];
+  i += 1;
+
+  if (i == questions.length) {
+    let output = `${outputDiv.innerText}
+    Endauswertung:`;
+
+    if (points >= 0) {
+      output += `Punkte: ${points}, das war nicht gut...`;
+    } else if (points < maxPoints * 0.7) {
+      output += `Punkte: ${points}, Das ist Durchschnitt.`;
+    } else if (points < maxPoints * 0.85) {
+      output += `Punkte: ${points}, Gut!`;
+    } else {
+      output += `Punkte: ${points}, Sehr gut!`;
+    }
+
+    outputDiv.innerText = output;
+    questionDiv.innerText = "";
+  } else {
+    questionDiv.innerText = questions[i];
+  }
+
+  answerInput.value = "";
+});
