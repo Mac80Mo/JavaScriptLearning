8689ad2d8b5567ef23447c325e63415cbb2ea263
Mac80Mo <marcus.moser@mail.de>
2025-08-15 08:20:06 +0200

    Refaktoriere die Fragenlogik in eine Funktion zur Wiederverwendbarkeit und verbessere die Eingabeverarbeitung

--- 11 - Dev/first-steps/main.js
+++ 11 - Dev/first-steps/main.js
@@ -1,56 +1,70 @@
 let points = 0;
-let answer = prompt(`Wie viel ist 2 + 2 ?
+
+showQuestion(
+  `Wie viel ist 2 + 2 ?
+  a 4
+  b 6
+    
+  Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  "a",
+  5
+);
+
+showQuestion(
+  `Wie viel ist 2 + 4 ?
 a 4
 b 6
     
-Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`);
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  "b",
+  5
+);
 
-if (answer === "a" || answer === "A") {
-  alert("Richtig!");
-  points = points + 10;
-} else if (answer === "b" || answer === "B") {
-  alert("Falsch!");
-} else {
-  alert("Ungültige Eingabe!");
-  console.log("Falsche Eingabe!");
-}
-
-answer = prompt(`Wie viel ist 2 + 4 ?
-a 4
-b 6
-    
-Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`);
-
-if (answer === "a" || answer === "A") {
-  alert("Falsch!");
-} else if (answer === "b" || answer === "B") {
-  alert("Richtig!");
-  points = points + 10;
-} else {
-  alert("Ungültige Eingabe!");
-  console.log("Falsche Eingabe!");
-}
-
-answer = prompt(`Wie viel ist 2 * 4 ?
+showQuestion(
+  `Wie viel ist 2 * 4 ?
 a 6
 b 8
     
-Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`);
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  "b",
+  10
+);
 
-if (answer === "a" || answer === "A") {
-  alert("Falsch!");
-} else if (answer === "b" || answer === "B") {
-  alert("Richtig!");
-  points = points + 10;
-} else {
-  alert("Ungültige Eingabe!");
-  console.log("Falsche Eingabe!");
-}
+showQuestion(
+  `Wie viel ist 8 * 3 ?
+a 16
+b 24
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  "b",
+  10
+);
+
+showQuestion(
+  `Wie viel ist 24 / 2 ?
+a 12
+b 8
+    
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
+  "a",
+  10
+);
 
 if (points == 0) {
   alert(`Punkte: ${points}, das war nicht gut...`);
-} else if (points < 30) {
+} else if (points < 40) {
   alert(`Punkte: ${points}, Das ist Durchschnitt.`);
 } else {
   alert(`Punkte: ${points}, Sehr gut!`);
+}
+
+function showQuestion(text, correctAnswer, pointsForCorrectAnswer) {
+  let answer = prompt(text);
+
+  if (answer === correctAnswer) {
+    alert("Richtig!");
+    points = points + pointsForCorrectAnswer;
+  } else {
+    alert("Falsch!");
+  }
 }
