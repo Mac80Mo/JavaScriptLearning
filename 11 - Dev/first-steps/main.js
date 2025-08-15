4eb0f4a3834a6b5aef8e96709201ede7d79ab226
Mac80Mo <marcus.moser@mail.de>
2025-08-15 18:36:05 +0200

    Initialize frage-antwort-html project with Vite setup, add SVG assets, and implement counter functionality

--- 11 - Dev/first-steps/main.js
+++ 11 - Dev/first-steps/main.js
@@ -1,59 +1,47 @@
 let points = 0;
-
-showQuestion(
+const questions = [
   `Wie viel ist 2 + 2 ?
-  a 4
-  b 6
+a 4
+b 6
     
-  Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
-  "a",
-  5
-);
-
-showQuestion(
+Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
   `Wie viel ist 2 + 4 ?
 a 4
 b 6
     
 Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
-  "b",
-  5
-);
-
-showQuestion(
   `Wie viel ist 2 * 4 ?
 a 6
 b 8
     
 Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
-  "b",
-  10
-);
-
-showQuestion(
   `Wie viel ist 8 * 3 ?
 a 16
 b 24
     
 Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
-  "b",
-  10
-);
-
-showQuestion(
   `Wie viel ist 24 / 2 ?
 a 12
 b 8
     
 Geben Sie den Buchstaben ein, welcher vor der richtigen Antwort steht.`,
-  "a",
-  10
-);
+];
+
+const correctAnswers = ["a", "b", "b", "b", "a"];
+const pointsForCorrectAnswers = [5, 5, 10, 10, 10];
+let maxPoints = 0;
+
+for (let i = 0; i < questions.length; i++) {
+  showQuestion(questions[i], correctAnswers[i], pointsForCorrectAnswers[i]);
+  maxPoints = maxPoints + pointsForCorrectAnswers[i];
+}
 
 if (points == 0) {
   alert(`Punkte: ${points}, das war nicht gut...`);
-} else if (points < 40) {
+} else if (points < maxPoints * 0.7) {
   alert(`Punkte: ${points}, Das ist Durchschnitt.`);
+} else if (points < maxPoints * 0.85) {
+  alert(`Punkte: ${points}, Gut!`);
 } else {
   alert(`Punkte: ${points}, Sehr gut!`);
 }
