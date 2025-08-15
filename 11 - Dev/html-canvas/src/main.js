144d43ff8469f1b8fd5a80b5110e5e556327b18d
Mac80Mo <marcus.moser@mail.de>
2025-08-15 21:13:47 +0200

    Initialize html-canvas project with Vite setup, add SVG assets, implement counter functionality, and create basic canvas drawing

---
+++ 11 - Dev/html-canvas/src/main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("main-canvas");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#333";
+context.fillRect(0, 0, 100, 100);
+context.fillStyle = "#333";
+context.fillRect(250, 0, 100, 100);
