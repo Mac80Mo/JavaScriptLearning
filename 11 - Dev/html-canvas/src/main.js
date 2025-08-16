232d83eb19bae031b9ad05ecf3d4e5e9494290c1
Mac80Mo <marcus.moser@mail.de>
2025-08-16 22:35:54 +0200

    Füge Tastatursteuerung für die Bewegungsrichtung hinzu und passe die Zeichenlogik an

--- 11 - Dev/html-canvas/src/main.js
+++ 11 - Dev/html-canvas/src/main.js
@@ -2,14 +2,24 @@ const canvas = document.getElementById("main-canvas");
 const context = canvas.getContext("2d");
 
 let i = 0;
+let dir = 1;
+
+document.addEventListener("keydown", (event) => {
+  console - log(event.code);
+  if (event.code == "ArrowLeft") {
+    dir = -1 * dir;
+  } else if (event.code == "ArrowRight") {
+    dir = 1;
+  }
+});
 
 draw();
 
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = "#333";
-  context.fillRect(i, i, 100, 100);
-  i++;
+  context.fillRect(i, 100, 100, 100);
+  i += dir * 1;
 
   requestAnimationFrame(draw);
 }
