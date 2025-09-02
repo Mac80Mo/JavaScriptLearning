bec8281b96d832c0b1e6b962007d3be94069e0a6
Mac80Mo <marcus.moser@mail.de>
2025-09-02 18:23:58 +0200

    Filtere und zeige nur ausgewählte Kryptowährungen in der API-Antwort an, um die Ausgabe zu optimieren.

--- 11 - Dev/virtual-crypto-trader/src/main.js
+++ 11 - Dev/virtual-crypto-trader/src/main.js
@@ -1,9 +1,26 @@
 const data = await fetchData();
 console.log("Komplette API-Antwort:", data); // Gibt das gesamte Array aus
 
-for (const coinData of data) {
+const coinsToDisplay = ["BTC", "ETH", "SOL", "XRP", "XLM", "ADA"];
+const filteredData = data.filter((coinData) =>
+  coinsToDisplay.includes(coinData.symbol)
+);
+console.log(filteredData);
+
+for (const coinData of filteredData) {
   console.log("Coin:", coinData.symbol, "price $:", coinData.quotes.USD.price);
 }
+
+//for (const coinData of data) {
+//  if (coinsToDisplay.includes(coinData.symbol)) {
+//    console.log(
+//      "Coin:",
+//      coinData.symbol,
+//      "price $:",
+//      coinData.quotes.USD.price
+//    );
+//  }
+//}
 async function fetchData() {
   const response = await fetch(
     "https://api.coinpaprika.com/v1/tickers?quotes=USD"
