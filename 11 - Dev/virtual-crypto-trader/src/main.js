8114eb7eaf63d392b474a9668a4ee47dfc3d4b76
Mac80Mo <marcus.moser@mail.de>
2025-09-03 11:41:22 +0200

    Optimiere die HTML-Struktur und verbessere die Ausgabe der Kryptowährungsdaten durch Anpassungen in der Status- und Coinliste-Darstellung.

--- 11 - Dev/virtual-crypto-trader/src/main.js
+++ 11 - Dev/virtual-crypto-trader/src/main.js
@@ -1,34 +1,47 @@
 const coinsToDisplay = ["BTC", "ETH", "SOL", "XRP", "XLM", "ADA"];
+let balance = 100000;
 
 const data = await fetchData();
-let output = `<table>
+
+renderStatus(balance);
+renderTable(data);
+
+function renderStatus(_balance) {
+  const statusDiv = document.getElementById("status");
+  statusDiv.innerHTML = `<h2>Status</h2>
+  <p>Balance: ${_balance}</p>`;
+}
+
+function renderTable(_data) {
+  let output = `<table>
 <tr>
   <th>Symbol</th>
   <th>Price in USD</th>
 </tr>`;
 
-// Filtere die Daten und wende anschließend forEach an
-data
-  .filter((coinData) => coinsToDisplay.includes(coinData.symbol))
-  .forEach((coinData) => {
-    output =
-      output +
-      `<tr>
+  // Filtere die Daten und wende anschließend forEach an
+  data
+    .filter((coinData) => coinsToDisplay.includes(coinData.symbol))
+    .forEach((coinData) => {
+      output =
+        output +
+        `<tr>
         <td>${coinData.symbol}</td>
         <td>${coinData.quotes.USD.price}</td>
       </tr>`;
-    console.log(
-      "Coin:",
-      coinData.symbol,
-      "price $:",
-      coinData.quotes.USD.price
-    );
-  });
+      console.log(
+        "Coin:",
+        coinData.symbol,
+        "price $:",
+        coinData.quotes.USD.price
+      );
+    });
 
-output += "</table>";
+  output += "</table>";
 
-const appDiv = document.getElementById("coinlist");
-appDiv.innerHTML = output;
+  const appDiv = document.getElementById("coinlist");
+  appDiv.innerHTML = output;
+}
 
 async function fetchData() {
   const response = await fetch(
